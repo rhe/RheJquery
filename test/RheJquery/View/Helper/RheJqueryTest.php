fac150a189c70750a7dd1f46141d88952c9b3445
Robert Hennig <info@robcoding.de>
2012-12-13 14:40:40 +0100

    Added missing tests.

--- test/RheJquery/View/Helper/RheJqueryTest.php
+++ test/RheJquery/View/Helper/RheJqueryTest.php
@@ -21,4 +21,50 @@ class RheJqueryTest extends TestCase {
 		$jquery->appendJavaScriptFile('/testPath/test.js');
 		$this->assertEquals(array('/testPath/test.js'), $jquery->getJavaScriptFiles());
 	}
+	
+	public function testPrependJavaScriptFile() {
+		$jquery = new RheJquery();
+		$jquery->prependJavaScriptFile('/second.js');
+		$jquery->prependJavaScriptFile('/first.js');
+		$this->assertEquals(array('/first.js', '/second.js'), $jquery->getJavaScriptFiles());
+	}
+	
+	public function testAppendDocumentReadySnippet() {
+		$jquery = new RheJquery();
+		$jquery->appendDocumentReadySnippet('firstSnippet');
+		$jquery->appendDocumentReadySnippet('secondSnippet');
+		$this->assertEquals(array('firstSnippet', 'secondSnippet'), $jquery->getDocumentReadySnippets());
+	}
+	
+	public function testPrependDocumentReadySnippet() {
+		$jquery = new RheJquery();
+		$jquery->prependDocumentReadySnippet('secondSnippet');
+		$jquery->prependDocumentReadySnippet('firstSnippet');
+		$this->assertEquals(array('firstSnippet', 'secondSnippet'), $jquery->getDocumentReadySnippets());
+	}
+	
+	public function testHead() {
+		$jquery = new RheJquery();
+		$jquery->appendCssFile('/test.css');
+		$jquery->appendJavaScriptFile('/test.js');
+		$jquery->appendDocumentReadySnippet('testSnippet');
+		
+		$expected = "<link href=\"/test.css\" media=\"screen\" rel\"stylesheet\" ".
+				"type=\"text/css\"/>\n";
+		$expected.= "<script type=\"text/javascript\" src=\"/test.js\"></script>\n";
+		$expected.= "<script type=\"text/javascript\">\n";
+		$expected.= "\t<!--\n";
+		$expected.= "\t".'${document}.ready(function() {'."\n";
+		$expected.= "\ttestSnippet\n\n";
+		$expected.= "\t".'});';
+		$expected.= "\t-->";
+		$expected.= "</script>\n";
+		
+		$this->assertEquals($expected, $jquery->head());
+	}
+	
+	public function testInvoke() {
+		$jquery = new RheJquery();
+		$this->assertEquals(get_class($jquery), get_class($jquery()));
+	}
 }
